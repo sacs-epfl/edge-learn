eaa33954dbc00b7712e3992696488b81fe7476f4
Zachary Doucet <zacharydoucet01@hotmail.com>
2023-07-25 10:30:14 -0400

    Cleaned up destination before saving (prevent any conflict)

--- src/edge_learn/save_most_recent_run.sh
+++ src/edge_learn/save_most_recent_run.sh
@@ -7,4 +7,5 @@ MOST_RECENT_DIR=$(ls -td -- "$BASE_DIR"/*/ | head -n 1)
 MOST_RECENT_DIR=${MOST_RECENT_DIR%/}
 
 echo "Saving images from ${MOST_RECENT_DIR}"
+rm ~/results/*
 cp $MOST_RECENT_DIR/edge_server/*.png ~/results
