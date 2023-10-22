c6aabb5d63133d7e7bd05fe11ac655271822d1c4
Zachary Doucet <zacharydoucet01@hotmail.com>
2023-10-22 11:41:09 -0400

    Another bug fix

--- src/edge_learn/training/TrainingGPU.py
+++ src/edge_learn/training/TrainingGPU.py
@@ -40,7 +40,7 @@ class Training:
             self.gpu_mapping = json.load(f)
 
         self.gpus_to_use = self.gpu_mapping.get(self.uid, [])
-        logging.debug("Using gpus: " + str.join(self.gpus_to_use))
+        logging.debug("Using gpus: " + ", ".join(self.gpus_to_use))
 
     def reset_optimizer(self, optimizer):
         self.optimizer = optimizer
