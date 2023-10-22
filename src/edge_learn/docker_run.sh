520b1c4b12bdc41196d2032048b70067ae2c4080
Zachary Doucet <zacharydoucet01@hotmail.com>
2023-10-22 09:57:38 -0400

    Fixes made to work on DGX and be able to run multiple of these setups on a single server by adding a name append argument on launch script

--- src/edge_learn/docker_run.sh
+++ src/edge_learn/docker_run.sh
@@ -10,7 +10,7 @@ create_primary_cloud() {
     local base_result_dir=$1
     mkdir -p $base_result_dir/primary_cloud
     echo "Running primary cloud"
-    docker run -d -p $(calculate_port -1):1000 -v $base_result_dir/primary_cloud:/results -v $2:/edge_learn -v $3:/decentralizepy -v $4:/data --shm-size=8g --name primary_cloud edge_learn:latest python3 create_node.py --node_type cloud --rank -1 --config_dir config 
+    docker run -d -p $(calculate_port -1):1000 -v $base_result_dir/primary_cloud:/results -v $2:/edge_learn -v $3:/decentralizepy -v $4:/data --shm-size=8g --name primary_cloud$5 edge_learn:latest python3 create_node.py --node_type cloud --rank -1 --config_dir config
 }
 
 is_primary_cloud() {
@@ -25,15 +25,15 @@ is_primary_cloud() {
 
 create_primary_cloud_if_needed() {
     if [ $(is_primary_cloud) -eq 0 ]; then
-        create_primary_cloud $1 $2 $3 $4
-    fi 
+        create_primary_cloud $1 $2 $3 $4 $5
+    fi
 }
 
 create_edge_server() {
     local base_result_dir=$1
     mkdir -p $base_result_dir/edge_server
     echo "Running edge server"
-    docker run -d -p $(calculate_port 0):1000 -v $base_result_dir/edge_server:/results -v $2:/edge_learn -v $3:/decentralizepy -v $4:/data --name edge_server edge_learn:latest python3 create_node.py --node_type edge --rank 0 --config_dir config
+    docker run -d -p $(calculate_port 0):1000 -v $base_result_dir/edge_server:/results -v $2:/edge_learn -v $3:/decentralizepy -v $4:/data --name edge_server$5 edge_learn:latest python3 create_node.py --node_type edge --rank 0 --config_dir config
 }
 
 create_client() {
@@ -41,14 +41,14 @@ create_client() {
     local base_result_dir=$2
     mkdir -p $base_result_dir/client_$i
     echo "Running client $i"
-    docker run -d -p $(calculate_port $((i + 1))):1000 -v $base_result_dir/client_$i:/results -v $3:/edge_learn -v $4:/decentralizepy -v $5:/data --name client_$i edge_learn:latest python3 create_node.py --node_type client --rank $((i + 1)) --config_dir config
+    docker run -d -p $(calculate_port $((i + 1))):1000 -v $base_result_dir/client_$i:/results -v $3:/edge_learn -v $4:/decentralizepy -v $5:/data --name client_$i$6 edge_learn:latest python3 create_node.py --node_type client --rank $((i + 1)) --config_dir config
 }
 
 create_clients() {
     local base_result_dir=$1
     local clients_per_machine=$(jq -r '.clients_per_machine' config/params.json)
     for i in $(seq 0 $(($clients_per_machine - 1))); do
-        create_client $i $base_result_dir $2 $3 $4
+        create_client $i $base_result_dir $2 $3 $4 $5
     done
 }
 
@@ -58,9 +58,9 @@ launch_nodes() {
     decentralizepy_dir=$(pwd)/../../decentralizepy/src/decentralizepy
     mkdir -p $base_result_dir
 
-    create_primary_cloud_if_needed $base_result_dir $edge_learn_dir $decentralizepy_dir $1
-    create_edge_server $base_result_dir $edge_learn_dir $decentralizepy_dir $1
-    create_clients $base_result_dir $edge_learn_dir $decentralizepy_dir $1
+    create_primary_cloud_if_needed $base_result_dir $edge_learn_dir $decentralizepy_dir $1 $2
+    create_edge_server $base_result_dir $edge_learn_dir $decentralizepy_dir $1 $2
+    create_clients $base_result_dir $edge_learn_dir $decentralizepy_dir $1 $2
 }
 
 wait_for_exit() {
@@ -73,24 +73,29 @@ wait_for_exit() {
 
 cleanup() {
     if [ $(is_primary_cloud) -eq 0 ]; then
-        wait_for_exit primary_cloud
+        wait_for_exit primary_cloud$1
     fi
-    wait_for_exit edge_server
+    wait_for_exit edge_server$1
     local clients_per_machine=$(jq -r '.clients_per_machine' config/params.json)
     for i in $(seq 0 $(($clients_per_machine - 1))); do
-        wait_for_exit client_$i
+        wait_for_exit client_$i$1
     done
 
     echo "All containers exited"
 }
 
 main() {
-    launch_nodes $1
-    cleanup
+    if [[ -z $2 ]]; then
+        name_append=""
+    else
+        name_append="_$2"
+    fi
+    launch_nodes $1 $name_append
+    cleanup $name_append
 }
 
-main $1
-# First and only argument is pointing to where dataset exists
+main $1 $2
+# First and only argument is pointing to where dataset exists, 2nd is optional specifying value to append to docker name
 
 
 
